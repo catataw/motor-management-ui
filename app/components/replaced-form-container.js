4e6201ed7d390ad898abfb1d5b56204a1a33a5c2
duel80003 <duel80003@gmail.com>
2018-10-08 23:28:42 +0800

    new component for replaced form

---
+++ app/components/replaced-form-container.js
@@ -0,0 +1,21 @@
+import Component from '@ember/component';
+import {connect} from 'ember-redux';
+
+class ReplacedFormContainer extends Component {}
+
+const stateToComputed = state => {
+  return {
+
+  }
+};
+
+const dispatchToActions = dispatch => {
+  return {
+
+  }
+};
+
+export default connect(
+  stateToComputed,
+  dispatchToActions,
+)(ReplacedFormContainer);
---
+++ app/components/replaced-form.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import { action, observes} from '@ember-decorators/object';
+
+export default class ReplacedFormComponent extends Component{
+
+};
